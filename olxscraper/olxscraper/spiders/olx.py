c8fc73d78691326ce89d467d74fc8d3341d64a1f
Abhicodes4 <163242319+Abhicodes4@users.noreply.github.com>
2024-07-01 13:22:38 +0530

    Update olx.py
    2nd commit

--- olxscraper/olxscraper/spiders/olx.py
+++ olxscraper/olxscraper/spiders/olx.py
@@ -14,3 +14,18 @@ class OlxSpider(scrapy.Spider):
         next_page = response.css('a[data-aut-id="btnLoadMore"]::attr(href)').get()
         if next_page:
             yield response.follow(next_page, self.parse)
+    
+    def parse_property(self, response):
+            yield {
+                'property_name': response.css('h1[data-aut-id="itemTitle"]::text').get(),
+                'property_id': response.css('div._1-oS0 ::text')[-2].get(),
+                'breadcrumbs': response.css('a._26_tZ::text').extract(),
+                'amount': response.css('span[data-aut-id="itemPrice"]::text').get(),
+                'image_url': ,
+                'description': ,
+                'seller_name': ,
+                'location': ,
+                'property_type': ,
+                'bathrooms': ,
+                'bedrooms': ,
+            }
